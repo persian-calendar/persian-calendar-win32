da84b91fe9b026d0ffe17a08d850d1e51f7ac8eb
Ebrahim <ebrahim@gnu.org>
2026-05-16 14:39:19 +0330

    Upgrade llvm version

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
 @echo off
 REM https://releases.llvm.org/download.html Install from the Windows installer, e.g. LLVM-18.1.8-win32.exe
-"C:\Program Files (x86)\LLVM\bin\clang" persian-calendar.cc -o persian-calendar.exe ^
+"C:\Program Files\LLVM\bin\clang" persian-calendar.cc -o persian-calendar.exe ^
     -Weverything -Wall -Wextra -Wpedantic -Werror -Weffc++ ^
     -Wno-c++98-compat-pedantic -Wno-nonportable-system-include-path -Wno-reserved-identifier ^
     -fno-exceptions -fno-rtti -Oz -lkernel32 -luser32 -lshell32 -lgdi32 -lshlwapi -ladvapi32 ^
-    -nostdlib -nodefaultlibs -nostartfiles -fuse-ld=lld-link ^
-    -Wl,/entry:start -Wl,/subsystem:windows ^
+    -nostdlib -nodefaultlibs -nostartfiles -fuse-ld=lld-link -m32 ^
+    -Wl,/entry:start -Wl,/subsystem:windows -Wl,-m32 ^
     -Wl,/fixed -Wl,/merge:.rdata=.text
