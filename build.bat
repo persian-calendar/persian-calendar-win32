d8ef6c8e9397f8ab6aef436db2e22330db266703
Ebrahim <ebrahim@gnu.org>
2026-05-16 14:47:07 +0330

    Remove extra option

--- build.bat
+++ build.bat
@@ -5,5 +5,5 @@ REM https://releases.llvm.org/download.html Install from the Windows installer, 
     -Wno-c++98-compat-pedantic -Wno-nonportable-system-include-path -Wno-reserved-identifier ^
     -fno-exceptions -fno-rtti -Oz -lkernel32 -luser32 -lshell32 -lgdi32 -lshlwapi -ladvapi32 ^
     -nostdlib -nodefaultlibs -nostartfiles -fuse-ld=lld-link -m32 ^
-    -Wl,/entry:start -Wl,/subsystem:windows -Wl,-m32 ^
+    -Wl,/entry:start -Wl,/subsystem:windows ^
     -Wl,/fixed -Wl,/merge:.rdata=.text
