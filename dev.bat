879f484cfe7e308568d3cf233c35d34d1e977ddb
Ebrahim <ebrahim@gnu.org>
2026-07-16 23:52:54 +0330

    Tweak dev script

--- dev.bat
+++ dev.bat
@@ -1,5 +1,6 @@
 @echo off
 taskkill /IM persian-calendar.exe /FI "STATUS eq RUNNING" ^
     && "C:\Program Files\LLVM\bin\clang" test.cc -D_CRT_SECURE_NO_WARNINGS -o test.exe && test.exe ^
-    && build.bat && python postlink.py && dir persian-calendar*.exe && persian-calendar.exe
+    && build.bat && python postlink.py && dir persian-calendar*.exe && start /b persian-calendar.exe ^
+    && pause && taskkill /IM persian-calendar.exe /FI "STATUS eq RUNNING"
 REM dumpbin /DISASM persian-calendar.exe
