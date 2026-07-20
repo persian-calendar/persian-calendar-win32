143e77c31dcf6062b63586e43f2eac1dce23ef99
Ebrahim <ebrahim@gnu.org>
2026-07-21 01:21:59 +0330

    Print more accurate exe size

--- dev.bat
+++ dev.bat
@@ -1,6 +1,8 @@
 @echo off
 taskkill /IM persian-calendar.exe /FI "STATUS eq RUNNING" ^
     && "C:\Program Files\LLVM\bin\clang" test.cc -D_CRT_SECURE_NO_WARNINGS -o test.exe && test.exe ^
-    && build.bat && python postlink.py && dir persian-calendar*.exe && start /b persian-calendar.exe ^
+    && build.bat && python postlink.py ^
+    && python -c "d=open('persian-calendar.exe', 'rb').read(); print(len(d.rstrip(b'\xcc')), '('+str(len(d))+')')" ^
+    && start /b persian-calendar.exe ^
     && pause && taskkill /IM persian-calendar.exe /FI "STATUS eq RUNNING"
 REM dumpbin /DISASM persian-calendar.exe
