35a87c2aedbe8ea961235184f366158b781661a8
Ebrahim <ebrahim@gnu.org>
2026-07-21 20:57:59 +0330

    Minor

--- dev.bat
+++ dev.bat
@@ -2,7 +2,7 @@
 taskkill /IM persian-calendar.exe /FI "STATUS eq RUNNING" ^
     && "C:\Program Files\LLVM\bin\clang" test.cc -D_CRT_SECURE_NO_WARNINGS -o test.exe && test.exe ^
     && build.bat && python postlink.py ^
-    && python -c "d=open('persian-calendar.exe', 'rb').read(); print(len(d.rstrip(b'\xcc')), '('+str(len(d))+')')" ^
+    && python -c "d=open('persian-calendar.exe', 'rb').read(); print(f'{len(d.rstrip(b'\xcc'))}/{len(d)}')" ^
     && start /b persian-calendar.exe ^
     && pause && taskkill /IM persian-calendar.exe /FI "STATUS eq RUNNING"
 REM dumpbin /DISASM persian-calendar.exe
