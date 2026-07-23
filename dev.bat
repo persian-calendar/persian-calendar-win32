02da547193a778dc66c9c00b3bee40c506a1a7cf
Ebrahim <ebrahim@gnu.org>
2026-07-23 18:00:34 +0330

    Consider for end padding becoming zero

--- dev.bat
+++ dev.bat
@@ -2,7 +2,7 @@
 taskkill /IM persian-calendar.exe /FI "STATUS eq RUNNING" ^
     && "C:\Program Files\LLVM\bin\clang" test.cc -D_CRT_SECURE_NO_WARNINGS -o test.exe && test.exe ^
     && build.bat && python postlink.py ^
-    && python -c "d=open('persian-calendar.exe', 'rb').read(); print(f'{len(d)}{len(d.rstrip(b'\xcc')) - len(d)}')" ^
+    && python -c "d=open('persian-calendar.exe', 'rb').read(); print(f'{len(d)}-{len(d) - len(d.rstrip(b'\xcc'))}')" ^
     && start /b persian-calendar.exe ^
     && pause && taskkill /IM persian-calendar.exe /FI "STATUS eq RUNNING"
 REM dumpbin /DISASM persian-calendar.exe
