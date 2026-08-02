67a1176d8611bcc347571b535a5b9a1e80477969
Ebrahim <ebrahim@gnu.org>
2026-08-02 17:07:06 +0330

    Remove -Wno-c++98-compat-pedantic

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 REM https://github.com/llvm/llvm-project/releases Install from the Windows installer, e.g. LLVM-22.1.8-win64.exe
 "C:\Program Files\LLVM\bin\clang" persian-calendar.cc -o persian-calendar.exe ^
     -Weverything -Wall -Wextra -Wpedantic -Werror -Weffc++ -std=c++2c ^
-    -Wno-c++98-compat-pedantic -Wno-c++17-attribute-extensions ^
+    -Wno-c++98-compat-pedantic ^
     -fno-exceptions -fno-rtti -fsafe-buffer-usage-suggestions -flto ^
     -Oz -lkernel32 -luser32 -lshell32 -lgdi32 -ladvapi32 ^
     -nostdlib -nodefaultlibs -nostartfiles -fuse-ld=lld-link -m32 ^
