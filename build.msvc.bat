c4119bf73c96f624a8b108390c65793aecf55e98
Ebrahim <ebrahim@gnu.org>
2026-08-03 01:01:51 +0330

    Minor

--- build.msvc.bat
+++ build.msvc.bat
@@ -1,7 +1,7 @@
 @echo off
 REM call "C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
 cl persian-calendar.cc /Fepersian-calendar.msvc.exe ^
-    /utf-8 /O1 /Wall /GS- /wd4710 /wd4711 /wd5045 /wd4820 /WX /std:c++latest /permissive- ^
+    /utf-8 /O1 /Wall /GS- /wd4710 /wd4711 /wd4820 /wd5045 /WX /std:c++latest /permissive- ^
     /external:anglebrackets /external:W0 ^
     /link /ENTRY:start /NODEFAULTLIB /SUBSYSTEM:WINDOWS /INCREMENTAL:NO ^
     kernel32.lib user32.lib shell32.lib gdi32.lib advapi32.lib ^
