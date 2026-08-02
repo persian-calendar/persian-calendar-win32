1b258c1d7e746534fc22334ca77d50ad0a79469a
Ebrahim <ebrahim@gnu.org>
2026-08-02 17:07:43 +0330

    Make gcc compile

--- build.gcc.bat
+++ build.gcc.bat
@@ -1,6 +1,6 @@
 @echo off
 REM https://github.com/niXman/mingw-builds-binaries/
 C:\mingw32\bin\i686-w64-mingw32-gcc persian-calendar.cc -o persian-calendar.gcc.exe ^
-    -Wall -Wextra -Wpedantic -Werror -Weffc++ -fno-exceptions -Oz -s ^
+    -Wall -Wextra -Wpedantic -Werror -Weffc++ -fno-exceptions -Oz -s -std=c++2c ^
     -lkernel32 -luser32 -lshell32 -lgdi32 -ladvapi32 ^
     -nostdlib -nodefaultlibs -nostartfiles -Wl,-e,start -Wl,-subsystem,windows
